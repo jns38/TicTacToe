9c652afc95afa660467541f62142660a1566a44c
Jessica Sellers <jns38@uw.edu>
2025-02-17 22:03:32 -0800

    Form1.Designer - change font Form1  - display "Player" if textbox is blank

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,148 +29,142 @@
         private void InitializeComponent()
         {
             tableLayoutPanel1 = new TableLayoutPanel();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
-            button9 = new Button();
-            restartBtn = new Button();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
             prompt = new Label();
             nameTextBox = new TextBox();
             startBtn = new Button();
             greetingLabel = new Label();
+            stopBtn = new Button();
+            exitBtn = new Button();
             tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.ColumnCount = 5;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.121212F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.12121F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25.2525253F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25.2525253F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25.2525253F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.121212F));
-            tableLayoutPanel1.Controls.Add(button1, 1, 3);
-            tableLayoutPanel1.Controls.Add(button2, 2, 3);
-            tableLayoutPanel1.Controls.Add(button3, 3, 3);
-            tableLayoutPanel1.Controls.Add(button4, 1, 4);
-            tableLayoutPanel1.Controls.Add(button5, 2, 4);
-            tableLayoutPanel1.Controls.Add(button6, 3, 4);
-            tableLayoutPanel1.Controls.Add(button7, 1, 5);
-            tableLayoutPanel1.Controls.Add(button8, 2, 5);
-            tableLayoutPanel1.Controls.Add(button9, 3, 5);
-            tableLayoutPanel1.Controls.Add(restartBtn, 3, 1);
+            tableLayoutPanel1.Controls.Add(btn1, 1, 3);
+            tableLayoutPanel1.Controls.Add(btn2, 2, 3);
+            tableLayoutPanel1.Controls.Add(btn3, 3, 3);
+            tableLayoutPanel1.Controls.Add(btn4, 1, 4);
+            tableLayoutPanel1.Controls.Add(btn5, 2, 4);
+            tableLayoutPanel1.Controls.Add(btn6, 3, 4);
+            tableLayoutPanel1.Controls.Add(btn7, 1, 5);
+            tableLayoutPanel1.Controls.Add(btn8, 2, 5);
+            tableLayoutPanel1.Controls.Add(btn9, 3, 5);
             tableLayoutPanel1.Controls.Add(prompt, 0, 0);
             tableLayoutPanel1.Controls.Add(nameTextBox, 2, 0);
             tableLayoutPanel1.Controls.Add(startBtn, 1, 1);
             tableLayoutPanel1.Controls.Add(greetingLabel, 1, 2);
-            tableLayoutPanel1.Location = new Point(1, 3);
+            tableLayoutPanel1.Controls.Add(stopBtn, 2, 1);
+            tableLayoutPanel1.Controls.Add(exitBtn, 3, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 7;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.930693F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.930693F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.930693F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 24.7524757F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 24.7524757F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 24.7524757F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 4.95049524F));
-            tableLayoutPanel1.Size = new Size(421, 379);
+            tableLayoutPanel1.RowCount = 8;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.58178234F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 7.94238567F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5.94135761F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 22.1236362F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 22.1236362F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 22.1236362F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.581782F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.581782F));
+            tableLayoutPanel1.Size = new Size(444, 438);
             tableLayoutPanel1.TabIndex = 0;
             // 
-            // button1
+            // btn1
             // 
-            button1.Location = new Point(54, 81);
-            button1.Name = "button1";
-            button1.Size = new Size(100, 87);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btn1.Location = new Point(56, 91);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(106, 90);
+            btn1.TabIndex = 0;
+            btn1.Text = "button1";
+            btn1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btn2
             // 
-            button2.Location = new Point(160, 81);
-            button2.Name = "button2";
-            button2.Size = new Size(100, 87);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            btn2.Location = new Point(168, 91);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(106, 90);
+            btn2.TabIndex = 1;
+            btn2.Text = "button2";
+            btn2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btn3
             // 
-            button3.Location = new Point(266, 81);
-            button3.Name = "button3";
-            button3.Size = new Size(100, 87);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            btn3.Location = new Point(280, 91);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(106, 90);
+            btn3.TabIndex = 2;
+            btn3.Text = "button3";
+            btn3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btn4
             // 
-            button4.Location = new Point(54, 174);
-            button4.Name = "button4";
-            button4.Size = new Size(100, 87);
-            button4.TabIndex = 3;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
+            btn4.Location = new Point(56, 187);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(106, 90);
+            btn4.TabIndex = 3;
+            btn4.Text = "button4";
+            btn4.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btn5
             // 
-            button5.Location = new Point(160, 174);
-            button5.Name = "button5";
-            button5.Size = new Size(100, 87);
-            button5.TabIndex = 4;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
+            btn5.Location = new Point(168, 187);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(106, 90);
+            btn5.TabIndex = 4;
+            btn5.Text = "button5";
+            btn5.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btn6
             // 
-            button6.Location = new Point(266, 174);
-            button6.Name = "button6";
-            button6.Size = new Size(100, 87);
-            button6.TabIndex = 5;
-            button6.Text = "button6";
-            button6.UseVisualStyleBackColor = true;
+            btn6.Location = new Point(280, 187);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(106, 90);
+            btn6.TabIndex = 5;
+            btn6.Text = "button6";
+            btn6.UseVisualStyleBackColor = true;
             // 
-            // button7
+            // btn7
             // 
-            button7.Location = new Point(54, 267);
-            button7.Name = "button7";
-            button7.Size = new Size(100, 87);
-            button7.TabIndex = 6;
-            button7.Text = "button7";
-            button7.UseVisualStyleBackColor = true;
+            btn7.Location = new Point(56, 283);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(106, 90);
+            btn7.TabIndex = 6;
+            btn7.Text = "button7";
+            btn7.UseVisualStyleBackColor = true;
             // 
-            // button8
+            // btn8
             // 
-            button8.Location = new Point(160, 267);
-            button8.Name = "button8";
-            button8.Size = new Size(100, 87);
-            button8.TabIndex = 7;
-            button8.Text = "button8";
-            button8.UseVisualStyleBackColor = true;
+            btn8.Location = new Point(168, 283);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(106, 90);
+            btn8.TabIndex = 7;
+            btn8.Text = "button8";
+            btn8.UseVisualStyleBackColor = true;
             // 
-            // button9
+            // btn9
             // 
-            button9.Location = new Point(266, 267);
-            button9.Name = "button9";
-            button9.Size = new Size(100, 87);
-            button9.TabIndex = 8;
-            button9.Text = "button9";
-            button9.UseVisualStyleBackColor = true;
-            // 
-            // restartBtn
-            // 
-            restartBtn.Anchor = AnchorStyles.None;
-            restartBtn.Location = new Point(266, 29);
-            restartBtn.Name = "restartBtn";
-            restartBtn.Size = new Size(100, 20);
-            restartBtn.TabIndex = 10;
-            restartBtn.Text = "Restart";
-            restartBtn.UseVisualStyleBackColor = true;
+            btn9.Location = new Point(280, 283);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(106, 90);
+            btn9.TabIndex = 8;
+            btn9.Text = "button9";
+            btn9.UseVisualStyleBackColor = true;
             // 
             // prompt
             // 
@@ -178,27 +172,30 @@
             prompt.AutoSize = true;
             tableLayoutPanel1.SetColumnSpan(prompt, 2);
             prompt.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            prompt.Location = new Point(10, 3);
+            prompt.Location = new Point(11, 3);
             prompt.Margin = new Padding(3);
             prompt.Name = "prompt";
-            prompt.Size = new Size(137, 20);
+            prompt.Padding = new Padding(5, 0, 0, 0);
+            prompt.Size = new Size(142, 21);
             prompt.TabIndex = 11;
             prompt.Text = "Enter your name: ";
             // 
             // nameTextBox
             // 
-            tableLayoutPanel1.SetColumnSpan(nameTextBox, 2);
-            nameTextBox.Location = new Point(160, 3);
+            tableLayoutPanel1.SetColumnSpan(nameTextBox, 3);
+            nameTextBox.Location = new Point(168, 5);
+            nameTextBox.Margin = new Padding(3, 5, 3, 3);
             nameTextBox.Name = "nameTextBox";
-            nameTextBox.Size = new Size(206, 23);
+            nameTextBox.Size = new Size(249, 23);
             nameTextBox.TabIndex = 12;
             // 
             // startBtn
             // 
             startBtn.Anchor = AnchorStyles.None;
-            startBtn.Location = new Point(54, 29);
+            startBtn.Location = new Point(59, 33);
+            startBtn.Margin = new Padding(3, 5, 3, 3);
             startBtn.Name = "startBtn";
-            startBtn.Size = new Size(100, 20);
+            startBtn.Size = new Size(100, 26);
             startBtn.TabIndex = 9;
             startBtn.Text = "Start";
             startBtn.UseVisualStyleBackColor = true;
@@ -209,19 +206,48 @@
             greetingLabel.AutoSize = true;
             tableLayoutPanel1.SetColumnSpan(greetingLabel, 3);
             greetingLabel.Dock = DockStyle.Fill;
-            greetingLabel.Location = new Point(54, 55);
+            greetingLabel.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            greetingLabel.ForeColor = Color.Black;
+            greetingLabel.Location = new Point(56, 65);
             greetingLabel.Margin = new Padding(3);
             greetingLabel.Name = "greetingLabel";
-            greetingLabel.Size = new Size(312, 20);
+            greetingLabel.Size = new Size(330, 20);
             greetingLabel.TabIndex = 13;
             greetingLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // stopBtn
+            // 
+            stopBtn.Anchor = AnchorStyles.None;
+            stopBtn.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            stopBtn.Location = new Point(171, 33);
+            stopBtn.Margin = new Padding(3, 5, 3, 3);
+            stopBtn.Name = "stopBtn";
+            stopBtn.Size = new Size(100, 26);
+            stopBtn.TabIndex = 10;
+            stopBtn.Text = "Stop";
+            stopBtn.UseVisualStyleBackColor = true;
+            stopBtn.Click += stopBtn_Click;
+            // 
+            // exitBtn
+            // 
+            exitBtn.Location = new Point(280, 33);
+            exitBtn.Margin = new Padding(3, 5, 3, 3);
+            exitBtn.Name = "exitBtn";
+            exitBtn.Size = new Size(100, 26);
+            exitBtn.TabIndex = 14;
+            exitBtn.Text = "Exit";
+            exitBtn.UseVisualStyleBackColor = true;
+            exitBtn.Click += exitBtn_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(422, 382);
+            AutoSize = true;
+            ClientSize = new Size(444, 438);
             Controls.Add(tableLayoutPanel1);
+            MaximumSize = new Size(517, 500);
+            MinimumSize = new Size(438, 421);
             Name = "Form1";
             Text = "Form1";
             tableLayoutPanel1.ResumeLayout(false);
@@ -232,19 +258,20 @@
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
-        private Button button8;
-        private Button button9;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
         private Button startBtn;
-        private Button restartBtn;
+        private Button stopBtn;
         private Label prompt;
         private TextBox nameTextBox;
         private Label greetingLabel;
+        private Button exitBtn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,22 +1,98 @@
+using System.Data.SqlTypes;
+
 namespace TicTacToe
 {
     public partial class Form1 : Form
     {
         private string playerName;
+        private Random rand = new Random();
+        private bool isPlayerXTurn = true;
+        private string playerMarker, computerMarker;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Gets name from user and greets them.
+        /// </summary>
+        /// <param name="sender">The button that was clicked, passed as an object</param>
+        /// <param name="e">Event argument for button click event</param>
         private void startBtn_Click(object sender, EventArgs e)
         {
             // Get the name from TextBox
-            playerName = nameTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(nameTextBox.Text))
+            {
+                playerName = nameTextBox.Text;
+            }
+            else
+            {
+                playerName = "Player"; // Default name if not exist
+            }
 
             // Display the greeting message with name on form
             greetingLabel.Text = ($"Welcome, {playerName}! Let's play Tic Tac Toe!");
 
+            // Start the game (enables Tic Tac Toe board)
+            StartGame();
         }
 
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // Closes the form
+        }
+
+        /// <summary>
+        /// Reset board without closing form
+        /// </summary>
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Enbles Tic Tac Toe board
+        /// 
+        /// 
+        /// </summary>
+        private void StartGame()
+        {
+            // Determine first player
+            FirstPlayer();
+
+            if (!isPlayerXTurn) return; // player can't input during computer's turn
+
+        }
+
+        /// <summary>
+        /// Randomly generates first player and assign them as X
+        /// </summary>
+        private void FirstPlayer()
+        {
+
+            // playerName is first if generate 0, 
+            // else generated 1 and computer is first 
+            if (isPlayerXTurn = rand.Next(2) == 0) // equal chance of turn
+            {
+                isPlayerXTurn = true;
+                playerMarker = "X";
+                computerMarker = "O";
+                greetingLabel.Text = $"{playerName} goes first as X.";
+            }
+            else
+            {
+                isPlayerXTurn = false;
+                computerMarker = "X";
+                playerName = "O";
+                greetingLabel.Text = "Computer goes first as X.";
+                ComputerMove();
+            }
+        }
+
+        private void ComputerMove()
+        {
+            
+        }
     }
 }
